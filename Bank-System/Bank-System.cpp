f391bfe7f5e09c1adc1f60beca0ec5b57591516f
Abd_Elrahman Mohamed <abdo17714@gmail.com>
2024-03-28 21:45:55 +0200

    Phase 1

--- Bank-System/Bank-System.cpp
+++ Bank-System/Bank-System.cpp
@@ -6,18 +6,14 @@
 using namespace std;
 int main()
 {
-    Client client(1, "Mohamed66", "password", 2000);
+    Client client(101, "Mohamed", "password", 1500);
     client.display();
-    client.deposit(500);
-    client.checkBalance();
-    client.withdraw(1000);
-    client.checkBalance();
     cout << "\n==================================\n" << endl;
 
-    Employee employee(101, "AbdElrahman", "password", 6000);
+    Employee employee(201, "AbdElrahman", "password", 6000);
     employee.display();
     cout << "\n==================================\n" << endl;
 
-    Admin admin(201, "AdminUser", "adminpass", 10000);
+    Admin admin(301, "AdminUser", "adminpass", 10000);
     admin.display();
 }
